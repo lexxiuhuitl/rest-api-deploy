80fa0982a3a7237847793d1a6d644aee8b55610c
lexxiuhuitl <lexartdev@gmail.com>
2023-10-02 02:13:39 -0600

    asent

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ app.use(express.json())
 app.use(cors({
   origin: (origin, callback) => {
     const ACCEPTED_ORIGINS = [
-      'http://localhost:1234',
       'http://localhost:8080',
-      'http://movies.com',
-      'http://midu.dev'
+      'http://localhost:1234',
+      'https://movies.com',
+      'https://midu.dev'
     ]
-    
+
     if (ACCEPTED_ORIGINS.includes(origin)) {
       return callback(null, true)
     }
@@ -27,9 +27,15 @@ app.use(cors({
     return callback(new Error('Not allowed by CORS'))
   }
 }))
-app.disable('x-powered-by')
+app.disable('x-powered-by') // deshabilitar el header X-Powered-By: Express
 
-//  Todos los recursos que sean MOVIES se identifican con /movies
+// métodos normales: GET/HEAD/POST
+// métodos complejos: PUT/PATCH/DELETE
+
+// CORS PRE-Flight
+// OPTIONS
+
+// Todos los recursos que sean MOVIES se identifica con /movies
 app.get('/movies', (req, res) => {
   const { genre } = req.query
   if (genre) {
@@ -45,21 +51,25 @@ app.get('/movies/:id', (req, res) => {
   const { id } = req.params
   const movie = movies.find(movie => movie.id === id)
   if (movie) return res.json(movie)
-  res.status(404).json({ message: 'movie not found' })
+  res.status(404).json({ message: 'Movie not found' })
 })
 
 app.post('/movies', (req, res) => {
   const result = validateMovie(req.body)
 
-  if (result.error) {
+  if (!result.success) {
+    // 422 Unprocessable Entity
     return res.status(400).json({ error: JSON.parse(result.error.message) })
   }
 
+  // en base de datos
   const newMovie = {
-    id: crypto.randomUUID(),
+    id: crypto.randomUUID(), // uuid v4
     ...result.data
   }
 
+  // Esto no sería REST, porque estamos guardando
+  // el estado de la aplicación en memoria
   movies.push(newMovie)
 
   res.status(201).json(newMovie)
@@ -101,7 +111,6 @@ app.patch('/movies/:id', (req, res) => {
 
   return res.json(updateMovie)
 })
-
 
 const PORT = process.env.PORT ?? 1234
 
